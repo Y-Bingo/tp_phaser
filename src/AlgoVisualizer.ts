a7a03ea846b6be1be315ce4b75592bb93969b5f9
Y-Bingo <y_bingo@163.com>
2019-03-29 15:21:56 +0800

    扫雷核心逻辑的实现 * 使用floorFill算法，解决点击无雷区域向外扩散展开 * 实现游戏的失败条件 * 实现游戏的胜利条件

--- src/AlgoVisualizer.ts
+++ src/AlgoVisualizer.ts
@@ -8,8 +8,14 @@ class AlgoVisualizer extends Phaser.Scene
 {
     private _sweeperData: SweeperData;          // 数据层
     private _algoFrame: AlgoFrame;              // 视图层
+
+    private _isGameOver: boolean;               // 是否游戏结束
+    private _isWin: boolean;                    // 是否胜利
     preload (): void
     {
+        this._isGameOver = false;
+        this._isWin = false;
+
         this.load.atlas( "sweeper", "resource/assest/sweeper.png", "resource/assest/sweeper.json" );
     }
     // 初始化视图
@@ -17,8 +23,6 @@ class AlgoVisualizer extends Phaser.Scene
     {
         this._sweeperData = new SweeperData( 10, 10, 10 );
         this._algoFrame = new AlgoFrame( this );
-
-        console.log( this._sweeperData );
 
         this._algoFrame.setMap( this._sweeperData );
 
@@ -31,25 +35,34 @@ class AlgoVisualizer extends Phaser.Scene
         this._algoFrame.clear();
         this._algoFrame.render( this._sweeperData );
     }
+
     // 鼠标监听
     private _initMouseInput (): void
     {
         this.input.mouse.disableContextMenu();
         this._algoFrame.setInteractive();
-        let row = 0;
-        let col = 0;
+
         this._algoFrame.on( 'pointerdown', ( pointer, x, y ) =>
         {
             let pos = this._algoFrame.transToIndex( x, y );
             // 右键
             if ( pointer.rightButtonDown() ) {
-                this._sweeperData.setFlag( pos.row, pos.col );
+                this._isWin = this._sweeperData.setFlag( pos.row, pos.col );
+                if ( this._isWin ) {
+                    this._algoFrame.off( "pointerdown" );
+                    console.log( "你赢了！" );
+                    this._sweeperData.openAll();
+                }
             }
             // 左键
             else {
-                this._sweeperData.open( pos.row, pos.col );
+                this._isGameOver = this._sweeperData.open( pos.row, pos.col );
+                if ( this._isGameOver ) {
+                    this._algoFrame.off( "pointerdown" );
+                    console.log( "游戏结束！" );
+                    this._sweeperData.openAll();
+                }
             }
-
         }, this );
     }
 }
--- src/SweeperData.ts
+++ src/SweeperData.ts
@@ -81,13 +81,13 @@ export class SweeperData
 
                 // 移除本身是雷
                 if ( this.isMine( row, col ) )
-                    this._nums[ row ][ col ] = -1;
+                    this._nums[ row ][ col ] = -8;
 
                 for ( let rr = row - 1; rr <= row + 1; rr++ ) {
                     for ( let cc = col - 1; cc <= col + 1; cc++ ) {
                         if ( !this.checkArea( rr, cc ) ) continue;
-                        if ( this.isMine( rr, cc ) )
-                            this._nums[ row ][ col ]++;
+                        if ( !this.isMine( rr, cc ) ) continue;
+                        this._nums[ row ][ col ]++;
                     }
                 }
             }
@@ -106,29 +106,67 @@ export class SweeperData
     isMine ( row: number, col: number ): boolean { return this.checkArea( row, col ) && this._map[ row ][ col ]; }
 
     // 立flag
-    setFlag ( row: number, col: number ): void
+    setFlag ( row: number, col: number ): boolean
     {
         if ( !this.checkArea( row, col ) ) return;
         if ( this._open[ row ][ col ] ) return;
 
         this._flag[ row ][ col ] = !this._flag[ row ][ col ];
+        return this.checkMineLeft() == 0;
     }
     // 是否立flag
     isFlag ( row: number, col: number ): boolean { return this.checkArea( row, col ) && this._flag[ row ][ col ]; }
 
+    checkMineLeft (): number
+    {
+        let resMine = this._mineNums;
+
+        for ( let i = 0; i < this._row; i++ )
+            for ( let j = 0; j < this._col; j++ )
+                if ( this._flag[ i ][ j ] && this._map[ i ][ j ] )
+                    resMine--;
+
+        return resMine;
+    }
+
+
     // 是否开啊
     isOpen ( row: number, col: number ): boolean { return this.checkArea( row, col ) && this._open[ row ][ col ]; }
-    // 打开某个格子
-    open ( row: number, col: number ): void
+    // 打开某个格子 如果开了炸弹，返回 true
+    open ( row: number, col: number ): boolean
     {
         // 越界不能打开
-        if ( !this.checkArea( row, col ) ) return;
+        if ( !this.checkArea( row, col ) ) return false;
         // 有旗帜不能打开
-        if ( this._flag[ row ][ col ] ) return;
+        if ( this._flag[ row ][ col ] ) return false;
+        // 已经打开过了不能打开
+        if ( this._open[ row ][ col ] ) return false;
 
         this._open[ row ][ col ] = true;
+
+        // 如果打开的是一个有数字的，则不泛红
+        if ( this._nums[ row ][ col ] > 0 ) return false;
+
+        // 有炸弹的不能打开
+        if ( this._map[ row ][ col ] ) return true;
+
+        for ( let i = row - 1; i <= row + 1; i++ ) {
+            for ( let j = col - 1; j <= col + 1; j++ ) {
+                this.open( i, j );
+            }
+        }
+    }
+    // 打开所有
+    openAll (): void
+    {
+        for ( let i = 0; i < this._row; i++ )
+            for ( let j = 0; j < this._col; j++ ) {
+                // 有旗帜的跳过
+                // if( this._flag[ i ][ j ] ) continue;
+                this._open[ i ][ j ] = true;
+            }
     }
 
     // 获取具体区域的炸弹数
-    getNums ( row: number, col: number ): number { return this._nums[ row ][ col ]; }
+    getNums ( row: number, col: number ): number { return Math.max( 0, this._nums[ row ][ col ] ); }
 }
